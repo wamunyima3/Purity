cdc04e715eb95e39bf337eb89316b046bc686394
wamunyima3 <wamunyimamukelabai3@gmail.com>
2024-05-28 17:11:34 +0200

    converted get url to .tsx

--- src/utils/getUrl.tsx
+++ src/utils/getUrl.tsx
@@ -1,8 +1,7 @@
-// utils/getURL.js
 export const getURL = () => {
     let url =
-      process.env.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-      process.env.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      process.env.NEXT_PUBLIC_SITE_URL ??
+      process.env.NEXT_PUBLIC_VERCEL_URL ??
       'http://localhost:3000/';
     
     url = url.includes('http') ? url : `https://${url}`;
