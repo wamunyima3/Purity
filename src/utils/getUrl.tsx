d2547b4ce562d37376b9ed4d05421555b6feaef7
wamunyima3 <wamunyimamukelabai3@gmail.com>
2024-05-28 17:15:27 +0200

    specified the return type

--- src/utils/getUrl.tsx
+++ src/utils/getUrl.tsx
@@ -1,12 +1,18 @@
-export const getURL = () => {
-    let url =
-      process.env.NEXT_PUBLIC_SITE_URL ??
-      process.env.NEXT_PUBLIC_VERCEL_URL ??
-      'http://localhost:3000/';
-    
-    url = url.includes('http') ? url : `https://${url}`;
-    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
-    
-    return url;
-  };
-  
+export const getURL = (): string => {
+  let url = 
+    process.env.NEXT_PUBLIC_SITE_URL ??
+    process.env.NEXT_PUBLIC_VERCEL_URL ??
+    'http://localhost:3000/';
+
+  // Ensure the URL starts with 'http'
+  if (!url.includes('http')) {
+    url = `https://${url}`;
+  }
+
+  // Ensure the URL ends with a '/'
+  if (url.charAt(url.length - 1) !== '/') {
+    url = `${url}/`;
+  }
+
+  return url;
+};
