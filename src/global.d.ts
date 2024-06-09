a8caf1a183b466e7b1abe6c3508161de30d04b13
wamunyima3 <wamunyimamukelabai3@gmail.com>
2024-06-09 13:16:26 +0200

    getting the url

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,8 +2,8 @@ declare namespace NodeJS {
     interface ProcessEnv {
       REACT_APP_SUPABASE_URL: string;
       REACT_APP_SUPABASE_KEY: string;
-      NEXT_PUBLIC_SITE_URL: string;
-      NEXT_PUBLIC_VERCEL_URL: string;
+      REACT_APP_NEXT_PUBLIC_SITE_URL: null;
+      REACT_APP_NEXT_PUBLIC_VERCEL_URL: null;
     }
   }
   
